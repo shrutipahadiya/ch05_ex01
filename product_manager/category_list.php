35034b740733524b704923f3f173e058af1c1220
sp2254 <sp2254@njit.edu>
2017-02-26 01:23:14 -0500

    added code for table for category name and delete button

--- product_manager/category_list.php
+++ product_manager/category_list.php
@@ -7,13 +7,24 @@
             <th>Name</th>
             <th>&nbsp;</th>
         </tr>        
-        <!-- add category rows here -->
-    </table>
+<!-- add category rows here -->
 
-    <h2>Add Category</h2>
-    <!-- add code for form here -->
+<!--Starts here-->
 
-    <p><a href="index.php?action=list_products">List Products</a></p>
-
+ <?php foreach ($categories as $category) : ?>
+<tr>
+<td><?php echo $category['categoryName']; ?></td>
+<td><form action="." method="post">
+<input type="hidden" name="action" value="delete_category">
+<input type="hidden" name="category_id" value="<?php echo $category['categoryID']; ?>">
+<input type="submit" value="Delete">
+</form>
+</td>
+</tr>
+<?php endforeach;?>
+ </table>
+ <h2>Add Category</h2>
+ <!-- add code for form here -->
+ <p><a href="index.php?action=list_products">List Products</a></p>
 </main>
 <?php include '../view/footer.php'; ?>
